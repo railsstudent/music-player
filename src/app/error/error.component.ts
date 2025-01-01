b3a422c35ebdc299fcd1006f7884e9fb8d5323ad
Connie Leung <cleung334@gmail.com>
2025-01-01 12:49:04 +0800

    refactor: use inline templates

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -2,7 +2,18 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-error',
-  templateUrl: './error.component.html',
+  template: `
+    @if (error()) {
+      <div class="bg-red-500 p-4 rounded">
+        <div class="flex items-center">
+          <span class="material-icons mr-2">error_outline</span>
+          <div>
+            <h4 class="font-bold">Error</h4>
+            <p>{{ error() }}</p>
+          </div>
+        </div>
+      </div>
+    }`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ErrorComponent {
